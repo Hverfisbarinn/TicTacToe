5f997f7d432ea24fdcdc448ccbb5200b4a0d7fdf
audunnhrafn <auddi123@gmail.com>
2017-10-25 19:03:07 +0000

    Created Board class

---
+++ src/main/java/is/ru/hugb/Board.java
@@ -0,0 +1,15 @@
+package is.ru.hugb;
+
+public class Board {
+	private char[] board;
+	
+	Board(){
+		board = new char[9];
+	}
+	
+	
+	
+	public static void main(String[] args) {
+		System.out.println("Hello World!");
+	}
+}
