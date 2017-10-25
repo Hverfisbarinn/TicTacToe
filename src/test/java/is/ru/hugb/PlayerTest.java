9e8a12ef611fe694b757c310ac01852fb24be292
OF-i <orn16@ru.is>
2017-10-25 19:43:36 +0000

    Added incrementWins function

--- src/test/java/is/ru/hugb/PlayerTest.java
+++ src/test/java/is/ru/hugb/PlayerTest.java
@@ -4,6 +4,18 @@ import org.junit.Test;
 import static org.junit.Assert.*;
 
 public class PlayerTest {
-
-	
+	@Test
+	public void testIncrementWins() {
+		Player p = new Player("Auddi", 'X');
+		p.incrementWins();
+		assertEquals(1, p.getWins());
+	}
+	@Test
+	public void testIncrementThreeWins() {
+		Player p = new Player("Auddi", 'X');
+		p.incrementWins();
+		p.incrementWins();
+		p.incrementWins();
+		assertEquals(3, p.getWins());
+	}
 }
