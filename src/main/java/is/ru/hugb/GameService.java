1d670534a764ab8214eb4b7e7a2bc92a4ad46981
trilli147 <Tristan@Tussa.local>
2017-10-25 23:37:51 +0000

    Added varibles and constructor in GameService

--- src/main/java/is/ru/hugb/GameService.java
+++ src/main/java/is/ru/hugb/GameService.java
@@ -1,5 +1,20 @@
 package is.ru.hugb;
 
 public class GameService {
+	private enum Status{
+		ACTIVE,
+		WIN,
+		DRAW; 
+	}
+	private Board board;
+	private Player player1;
+	private Player player2;
+
+	GameService(){
+		board = new Board();
+		player1 = new Player("Player 1", 'X');
+		player2 = new Player("Player 2", 'O');
+	}
+
 	
 }
