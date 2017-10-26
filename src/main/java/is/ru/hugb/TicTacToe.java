4dd7ffca7a1d8a952cea363e5df3f99a0f5efae7
audunnhrafn <auddi123@gmail.com>
2017-10-26 18:25:47 +0000

    heroku fix

--- src/main/java/is/ru/hugb/TicTacToe.java
+++ src/main/java/is/ru/hugb/TicTacToe.java
@@ -3,7 +3,19 @@ package is.ru.hugb;
 import static spark.Spark.*;
 
 public class TicTacToe {
+
 	public static void main(String[] args) {
+		port(getPort());
 		get("/", (req, res) -> "Hello World");
 	}
+
+	static int getPort() {
+		ProcessBuilder psb = new ProcessBuilder();
+		if (psb.environment().get("PORT") != null) {
+			return Integer.parseInt(psb.environment().get("PORT"));
+		}
+
+		return 4567;
+	}
+
 }
