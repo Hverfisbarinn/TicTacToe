de10c3b235b39ae4ad920d7e818e4cddb10a25cf
Tristan John <tristanjfz@gmail.com>
2017-10-27 09:14:38 +0000

    Added json object

--- src/main/java/is/ru/hugb/TicTacToe.java
+++ src/main/java/is/ru/hugb/TicTacToe.java
@@ -1,13 +1,38 @@
 package is.ru.hugb;
 
+import org.json.*;
 import static spark.Spark.*;
 
 public class TicTacToe {
+	private GameService service;
 
+	TicTacToe(){
+		service = new GameService();
+	}
+	
 	public static void main(String[] args) {
         staticFileLocation("/public");
-        get("/", (req, res) -> "");
         port(getHerokuAssignedPort());
+        TicTacToe game = new TicTacToe();
+        get("/makeMove/:move", (req, res) -> makeMove(req.params(":move")));
+	}
+	
+	private JSONObject makeMove(String move) {
+		JSONObject obj = new JSONObject();
+		obj.put("board", service.getBoard());
+		obj.put("player", service.getCurrentPlayer().getName());
+		
+		service.makeMove(Integer.parseInt(move));
+		Status gameStatus = service.getStatus();
+		if(gameStatus == Status.WIN) {
+			obj.put("status", "win");
+		}else if(gameStatus == Status.DRAW) {
+			obj.put("status", "draw");
+		}else {
+			obj.put("status", "inplay");
+		}
+		
+		return obj;
 	}
 
     static int getHerokuAssignedPort() {
