79ed87e53df4857d5364fa313c8bff43bab9b0d4
Tristan John <tristanjfz@gmail.com>
2017-10-27 09:29:06 +0000

    Added images when marking

--- src/main/resources/public/script.js
+++ src/main/resources/public/script.js
@@ -1,5 +1,13 @@
 $(document).ready(function(){
-	$("button").click(function(){
+    $("button").click(function(){
+		if(document.getElementById("currPlayer").innerHTML == "Player 1"){
+			$(this).css('background-image', 'url(img/x.png)');
+			document.getElementById("currPlayer").innerHTML = "Player 2";
+		}else{
+			$(this).css('background-image', 'url(img/o.png)');
+			document.getElementById("currPlayer").innerHTML = "Player 1";			
+		}
+		
 		$.ajax({url: "/makeMove/" + this.id, success: function(result) {
 			var object = JSON.parse(result);
 			var player = object.player;
